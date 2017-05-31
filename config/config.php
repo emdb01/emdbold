5bd50392e2dbe191473de99c983f673391d0bca1
rajamohan <maaapp@gmail.com>
2017-05-31 14:26:21 +0530

    live database

--- config/config.php
+++ config/config.php
@@ -3,16 +3,18 @@
 $offline=true;
 if(isset($online))
 {
-$servername='localhost';     // Your MySql Server Name or IP address here
-$dbusername='root';                // Login user id here
-$dbpassword='oNxwlyLMfOTebxLi';                // Login password here
+	$servername='localhost';     // Your MySql Server Name or IP address here
+	$dbusername='root';                // Login user id here
+	$dbpassword='';                // Login password here
+	$dbname='emdb';     // Your database name here
 }elseif(isset($offline))
 {
-$servername='ec2-23-23-228-115.compute-1.amazonaws.com';     // Your MySql Server Name or IP address here
-$dbusername='igejpvqxokuywq';                // Login user id here
-$dbpassword='a44221ce114876d34f5d5f02630f5ff069211987a1a80ca89c865b8edb3dec8a';                // Login password here	
-	}
-$dbname='d6ig0dt8ce3mnf';     // Your database name here
+	$servername='ec2-23-23-228-115.compute-1.amazonaws.com';     // Your MySql Server Name or IP address here
+	$dbusername='igejpvqxokuywq';                // Login user id here
+	$dbpassword='a44221ce114876d34f5d5f02630f5ff069211987a1a80ca89c865b8edb3dec8a';                // Login password here	
+	$dbname='d6ig0dt8ce3mnf';     // Your database name here
+}
+
 
 connecttodb($servername,$dbname,$dbusername,$dbpassword);
 function connecttodb($servername,$dbname,$dbuser,$dbpassword)
